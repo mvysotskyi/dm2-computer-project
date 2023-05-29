0525919c258de7a4af01a5142afe2d6a9c5f8734
dmytro <hamula.dm@gmail.com>
2023-05-29 12:05:12 +0300

    Fixed some bugs & added docs

--- visualization.py
+++ visualization.py
@@ -1,18 +1,26 @@
-"""
-Visualization module for Lenia using Pygame.
-"""
+'''
+Visualization module for Lenia using Pygame
+'''
 
+# Importing all of the necessary modules
 import numpy as np
-
 import pygame
 from lenia import Lenia
+import multiprocessing
+import imageio
+from PIL import Image
 
 
-class LeniaVisualizer:
-    """
-    Visualize Lenia using Pygame.
-    """
-    def __init__(self, lenia: Lenia, screen_width: int = 600, screen_height: int = 600):
+class LeniaVisualizer(object):
+    '''
+    Visualizing for LeniaVisualizer class using Pygame
+    '''
+    def __init__(self, lenia: Lenia, screen_width: int = 512, screen_height: int = 512):
+        '''
+        Initialization Visu class \
+
+        It initializaes of all the necessary attributes
+        '''
         self.lenia = lenia
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -20,36 +28,55 @@ class LeniaVisualizer:
         self.clock = pygame.time.Clock()
 
     def __draw(self):
-        self.screen.fill((0, 0, 0))
-        # Draw the world
+        pygame.display.set_caption('Lenia')
+        self.screen.fill((0, 0, 0)) # Makes black screen
+        # Draws the world
         cell_size = self.screen_width // self.lenia.size
-        world = np.dstack(self.lenia.world)
-
-        world *= 255
+        world = np.dstack(self.lenia.world) # Makes '3D-array for RGB visualization'
+        world *= 255 # Scalar multiplying
         surf = pygame.surfarray.make_surface(world)
         self.screen.blit(surf, (0, 0))
         # for x in range(self.lenia.size):
         #     for y in range(self.lenia.size):
         #         # color = world[x, y] * 255
         #         pygame.draw.rect(self.screen, tuple(world[x, y]), (x * cell_size, y * cell_size, cell_size, cell_size))
-
         pygame.display.update()
 
     def run(self):
-        """
-        Run the visualization.
-        """
-        while True:
+        '''
+        Main loop for the visualization
+        '''
+        frames = []
+        for _ in range(1000):
+
+        # while True:
             self.__draw()
             self.lenia.next()
             self.clock.tick(30)
+
+            frame = pygame.surfarray.array3d(self.screen)
+            frame = Image.fromarray(frame)
+            frames.append(frame)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+            # frame = self.screen.copy()
+            # frames.append(frame)
 
-if __name__ == "__main__":
-    _lenia = Lenia(pattern="emitter", size=800, scale=8, start_x=5, start_y=5)
-    visualizer = LeniaVisualizer(_lenia)
-    visualizer.run()
+        imageio.mimsave('lenia.gif', frames)
+
+        # pygame.quit()
+        # quit()
+
+
+if __name__ == '__main__':
+    _lenia = Lenia(pattern = 'pacman', size = 512, scale = 2, start_x = 5, start_y = 5)
+    visualizer = LeniaVisualizer(_lenia, screen_height = 512, screen_width = 512)
+
+    num_processes = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes = num_processes)
+    pool.map(visualizer.run(), range(num_processes))
+    pool.close()
+    pool.join()
